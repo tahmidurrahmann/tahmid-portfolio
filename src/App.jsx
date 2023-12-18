e20fffdf77c9c60c5e3860bc7606c01c1e0464d4
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-12-18 14:47:21 +0600

    adde navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { Outlet } from "react-router-dom"
-// import MainLayout from "./layout/MainLayout"
 import Footer from "./shared/Footer/Footer"
-import Navbarr from "./shared/Navbar/Navbarr"
+import Navbar from "./shared/Navbar/Navbar"
 
 function App() {
 
   return (
     <div className="font-raleway bg-black text-white">
-      {/* <MainLayout> */}
-      <Navbarr></Navbarr>
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
-      {/* </MainLayout> */}
     </div>
   )
 }
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,46 +1,120 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {  Container } from '@mui/material';
 import { Link } from 'react-scroll';
 
-const Navbar = () => {
-    return (
-        <div className="fixed w-full z-10">
-            <div className="max-w-screen-xl mx-auto navbar">
-                <div className="flex-none lg:hidden">
-                    <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                    </label>
-                </div>
-                <div className="flex-1 px-2 mx-2"><span className="flex font-permanent text-3xl items-center justify-center text-white"><img className="w-[60px]" src="https://i.ibb.co/3BPbCfh/21904da7d079ccffa6b95ecfc6f7bbb7-1.png" alt="" />Tahmid</span></div>
-                <div className="flex-none hidden lg:block">
-                    <div className="flex justify-center items-center gap-6 font-permanent text-white">
-                        <Link to="home" spy={true}
-                            smooth={true}
-                            offset={50}
-                            duration={500} className="hover:text-[#c60678]">Home</Link>
-                        <Link to="about" spy={true}
-                            smooth={true}
-                            offset={50}
-                            duration={500} className="hover:text-[#c60678]">About</Link>
-                        <Link to="skills" spy={true}
-                            smooth={true}
-                            offset={50}
-                            duration={500} className="hover:text-[#c60678]">Skills</Link>
-                        <Link to="projects" spy={true}
-                            smooth={true}
-                            offset={50}
-                            duration={500} className="hover:text-[#c60678]">Projects</Link>
-                        <Link to="education" spy={true}
-                            smooth={true}
-                            offset={50}
-                            duration={500} className="hover:text-[#c60678]">Education</Link>
-                        <Link to="contact" spy={true}
-                            smooth={true}
-                            offset={50}
-                            duration={500} className="hover:text-[#c60678]">Contact</Link>
-                    </div>
-                </div>
-            </div>
-        </div>
+const drawerWidth = 240;
+
+function Navbar(props) {
+
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const navLinks = <div className="flex flex-col md:flex-row justify-center items-center gap-12 md:gap-6 font-permanent">
+        <Link to="home" spy={true}
+            smooth={true}
+            offset={50}
+            duration={500} className="hover:text-[#c60678] mt-6 md:mt-0">Home</Link>
+        <Link to="about" spy={true}
+            smooth={true}
+            offset={50}
+            duration={500} className="hover:text-[#c60678]">About</Link>
+        <Link to="skills" spy={true}
+            smooth={true}
+            offset={50}
+            duration={500} className="hover:text-[#c60678]">Skills</Link>
+        <Link to="projects" spy={true}
+            smooth={true}
+            offset={50}
+            duration={500} className="hover:text-[#c60678]">Projects</Link>
+        <Link to="education" spy={true}
+            smooth={true}
+            offset={50}
+            duration={500} className="hover:text-[#c60678]">Education</Link>
+        <Link to="contact" spy={true}
+            smooth={true}
+            offset={50}
+            duration={500} className="hover:text-[#c60678]">Contact</Link>
+    </div>
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center'}}>
+            <Typography sx={{ my: 2 }}>
+            <div className='font-permanent text-3xl text-black flex items-center justify-center'><img className="w-[60px]" src="https://i.ibb.co/3BPbCfh/21904da7d079ccffa6b95ecfc6f7bbb7-1.png" alt="" />Tahmid</div>
+            </Typography>
+            <Divider />
+            <List>
+                {navLinks}
+            </List>
+        </Box>
     );
-};
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed" sx={{ backgroundColor: "#1515154D" }} component="nav">
+                <Container maxWidth="xl">
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                        >
+                            <div className='font-permanent text-3xl text-white flex items-center'><img className="w-[60px]" src="https://i.ibb.co/3BPbCfh/21904da7d079ccffa6b95ecfc6f7bbb7-1.png" alt="" />Tahmid</div>
+                        </Typography>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            {navLinks}
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+            <Box component="main">
+                <Toolbar />
+            </Box>
+        </Box>
+    );
+}
 
 export default Navbar;
