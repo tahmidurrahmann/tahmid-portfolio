183c90a176cb54ccc657970d57456b3bb387c210
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-12-23 14:30:38 +0600

    added up arrow

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,10 +4,15 @@ import About from "../../About/About";
 import Skills from "../../Skills/Skills";
 import Education from "../../Education/Education";
 import Projects from "../../Projects/Projects";
+import ScrollToTop from "react-scroll-up";
+import { FaArrowUp } from "react-icons/fa";
 
 const Home = () => {
     return (
         <div>
+            <ScrollToTop showUnder={160}>
+                <span><FaArrowUp className="text-[#c60678]" size={32}/></span>
+            </ScrollToTop>
             <Banner></Banner>
             <About></About>
             <Skills></Skills>
